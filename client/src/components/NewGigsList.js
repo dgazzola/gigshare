058c3b6796e0ccbf7fd99a0029315d8ba103b64a
Dan Gazzola <danielrgazzola@gmail.com>
2023-02-12 17:26:05 -0500

    uploading main

---
+++ client/src/components/NewGigsList.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const NewGigsList = () => {
+
+  return (
+    <div>
+      <h1>Hello from NewGigsList</h1>
+    </div>
+  )
+}
+
+export default NewGigsList
---
+++ client/src/components/TopGigsList.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const TopGigsList = () => {
+
+  return (
+    <div>
+      <h1>Hello from TopGigList</h1>
+    </div>
+  )
+}
+
+export default TopGigsList
