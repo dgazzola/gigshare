fe5b1591d87b22fe68e0e956124f34e5f90d8632
Dan Gazzola <danielrgazzola@gmail.com>
2024-11-12 19:39:32 -0500

    imported React to new component

--- client/src/components/ToggleGroup.js
+++ client/src/components/ToggleGroup.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const ToggleGroup = ({ setSelectedComponent, selectedComponent }) => {
   const [alignment, setAlignment] = useState(selectedComponent === "allGigs" ? "left" : "center");
